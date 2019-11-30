16011c8d48e2a4d68821839b4ed41530119e7463
CJ <weicongju@gmail.com>
2019-11-30 11:09:08 +0800

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get('/')
+async def hello(request):
+    return web.Response(text="Hello, world")
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app)
