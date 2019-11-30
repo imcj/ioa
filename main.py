3132d36fe3d9e2e565476e3491aa7c0cfadc4440
CJ <weicongju@gmail.com>
2019-11-30 11:30:17 +0800

    集成jinja2

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import os
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
 routes = web.RouteTableDef()
 
+
 @routes.get('/')
+@aiohttp_jinja2.template('index.html')
 async def hello(request):
-    return web.Response(text="Hello, world")
+    # return web.Response(text="Hello, world")
+    return {
+
+    }
 
 app = web.Application()
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader(os.path.abspath('./templates')))
+
 app.add_routes(routes)
 web.run_app(app)
