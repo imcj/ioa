c5667d3e5f91c3bb4675ef804f74e876c1de33b4
CJ <weicongju@gmail.com>
2019-11-30 12:00:36 +0800

    集成mysql

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import os
-from aiohttp import web
+import asyncio
 import aiohttp_jinja2
 import jinja2
+import aiomysql
+from aiohttp import web
 
 routes = web.RouteTableDef()
+loop = None
+pool = None
 
 
 @routes.get('/')
 @aiohttp_jinja2.template('index.html')
 async def hello(request):
-    # return web.Response(text="Hello, world")
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("SELECT 42;")
+            (r,) = await cur.fetchone()
     return {
 
     }
+
+loop = asyncio.get_event_loop()
+
+async def init_database():
+    global pool
+    config = {
+        "host": '127.0.0.1',
+        "port": 3306,
+        "user": "root",
+        "password": "",
+        "db": "test",
+        "loop": loop,
+        "maxsize": 100
+    }
+    pool = await aiomysql.create_pool(**config)
+
+loop.run_until_complete(init_database())
+
 
 app = web.Application()
 aiohttp_jinja2.setup(app,
